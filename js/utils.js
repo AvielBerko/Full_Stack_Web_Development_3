a5a2efab65c25de7a43048ee7b477c5a52cd2179
Aviel Berkowitz <9155988@gmail.com>
2023-02-28 18:45:10 +0200

    Moved uuid to utils

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,13 @@
 /*
 This module holds utility functions used accross the site.
 */
-function loginout() {
-    if (active_user != null) {
-        localStorage.removeItem('active_user');
-        location.reload();
-    }
-    else {
-        load_login_page(true);
-    }
+
+/**
+* This function generates a uniqe UUID for saving items in the database.
+* @returns Uniqe UUID.
+*/
+function generateUUID() {
+    const data = new Uint32Array(4);
+    crypto.getRandomValues(data);
+    return `${data[0].toString(16).padStart(8, '0')}-${data[1].toString(16).padStart(4, '0')}-${data[2].toString(16).padStart(4, '0')}-${data[3].toString(16).padStart(4, '0')}-${Math.floor(Math.random() * 0xffffffff).toString(16).padStart(8, '0')}`;
 }
